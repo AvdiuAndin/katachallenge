7ff740ee742edc0b68465a13267f0977e62ef319
Andin Avdiu <andiavdiu@gmail.com>
2019-05-09 21:38:58 +0200

    Update function updateQuality& Check functionality if the item is of type with updateState functionality implemented & add printItems functionality

--- app/golden-rose.ts
+++ app/golden-rose.ts
@@ -1,4 +1,9 @@
 import { Item } from "./Models/Item/Item";
+import { AgedBrie } from "./Models/Item/aged-brie-item";
+import { BackStageItem } from "./Models/Item/backstage-item";
+import { SulfurasItem } from "./Models/Item/sulfuras-item";
+import { ConjuredItem } from "./Models/Item/conjured-item";
+import { CasualItem } from "./Models/Item/casual-item";
 
 export class GildedRose {
     items: Array<Item>;
@@ -14,28 +19,53 @@ export class GildedRose {
         return arrayOfItems;
     }
 
-    private categorizeItemByName(item: Item): Item{
+    private categorizeItemByName(item: Item): Item {
         let name = item.name.toLocaleLowerCase();
 
         if(name.indexOf('aged brie') > -1){
-            // new Aged  Brie item
+            return new AgedBrie(item.name, item.sellIn, item.quality);
         }
 
         if(name.indexOf('backstage passes') > -1){
-            // new backstagge passes item
+            return new BackStageItem(item.name, item.sellIn, item.quality);
         }
 
         if(name.indexOf('sulfuras') > -1){
-            // new backstagge pass item
+            return new SulfurasItem(item.name, item.sellIn, item.quality);
         }
 
         if(name.indexOf('conjured') > -1) {
-            // new cojured item
+            return new ConjuredItem(item.name, item.sellIn, item.quality);
         }
+
+        return new CasualItem(item.name, item.sellIn, item.quality);
+        
     }
 
     updateQuality() {
+        for(let item of this.items){
+            this.decreaseSellIn(item);
+            if (this.itemImplementsUpdateStateInterface(item)) {
+                item.updateState();  // compiler knows it can update state now
+             }
+        }
         return this.items;
+    }
+
+    itemImplementsUpdateStateInterface(arg: any): arg is UpdateState {
+        return (arg as UpdateState).updateState !== undefined;
+    }
+     
+    printItems() {
+        console.log("#id, name, sellIn, quality");
+        this.items.forEach((element,i) => {
+            console.log(i + ':' + element.name + ' ' + element.sellIn + ' ' + element.quality);
+        });
+        console.log();
+    }
+
+    decreaseSellIn(item:Item){
+        item.sellIn--;
     }
 
 }
