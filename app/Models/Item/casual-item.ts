7709f67b93bf739bbf678580e24fc7293360a4e8
Andin Avdiu <andiavdiu@gmail.com>
2019-05-09 21:33:49 +0200

    Create Casual Item class &  Update categorize functionality to create casual items

---
+++ app/Models/Item/casual-item.ts
@@ -0,0 +1,25 @@
+import { constants } from './../../Constants/constants';
+import { Item } from "./item";
+
+export class CasualItem extends Item implements UpdateState {
+
+    constructor(
+        name: string,
+        sellIn: number,
+        quality: number){
+            super(name,sellIn,quality);
+        }
+
+        updateState(){
+        let substract;
+        if(this.sellIn < 0){
+            substract = this.quality - this.quality;
+        } else {
+            substract = this.quality - 1;
+        }
+        // quality is set to 0 if the calculation result is less than 0
+        this.quality = (substract) < constants.NORMAL_ITEM_MIN_QUALITY ? 0 : substract;
+        
+    }
+
+}
